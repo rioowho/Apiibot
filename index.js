6ef7d00d51c107888c0754d5280eb51387f9edfc
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-11-16 20:00:20 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const axios = require('axios');
 const fetch = require('node-fetch');
 const { randomBytes, randomUUID } = require('crypto');
+const { chromium } = require('playwright');
 const cheerio = require('cheerio');
 
 const model = "70b";
@@ -14,6 +15,40 @@ app.set("json spaces", 2);
 global.creator = "@riooxdzz"
 // Middleware untuk CORS
 app.use(cors());
+
+
+async function mediafire(url) {
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext({
+        userAgent: 'Mozilla/5.0 (Linux; Android 6.0; iris50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36'
+    });
+    const page = await context.newPage();
+    try {
+        await page.goto(url);
+        const downloadInfo = await page.evaluate(() => {
+            const fileNameElement = document.querySelector('.dl-btn-label');
+            const fileName = fileNameElement ? fileNameElement.textContent.trim() : '';
+            const downloadLinkElement = document.querySelector('#downloadButton');
+            const downloadLink = downloadLinkElement ? downloadLinkElement.href : '';
+            const fileSizeText = downloadLinkElement ? downloadLinkElement.textContent : '';
+            const sizeMatch = fileSizeText.match(/\(([^)]+)\)/);
+            const fileSize = sizeMatch ? sizeMatch[1] : '';
+
+            return {
+                fileName: fileName,
+                downloadLink: downloadLink,
+                fileSize: fileSize
+            };
+        });
+
+        return downloadInfo;
+    } catch (error) {
+        return { success: false, message: error.message };
+        console.error("Error:", error.response ? error.response.data : error.message);
+    } finally {
+        await browser.close();
+    }
+}
 async function tiktok(url) {
   return new Promise(async (resolve, reject) => {
     try {
@@ -544,6 +579,22 @@ app.get('/api/tiktok', async (req, res) => {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
     }
     const response = await tiktok(url);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get('/api/mediafire', async (req, res) => {
+  try {
+    const url = req.query.url;
+    if (!url) {
+      return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
+    }
+    const response = await mediafire(url);
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
