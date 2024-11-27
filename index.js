8bafacb78ead6c7c2bc623ef2d2b87b11d28b824
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-11-27 11:14:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -979,37 +979,38 @@ function generateRandomIP() {
     return `${octet()}.${octet()}.${octet()}.${octet()}`;
 }
 
-async function talkai(type, message) {
-    try {
-        const headers = {
-            'User-Agent': generateRandomUserAgent(),
-            'Referer': 'https://talkai.info/id/chat/',
-            'X-Forwarded-For': generateRandomIP(),
-        };
-
-        const data = {
-            temperature: 1,
-            frequency_penalty: 0,
-            type: type,
-            messagesHistory: [{
-                    from: 'chatGPT',
-                    content: 'Kamu adalah Rioo, kamu bukanlah ai atau chat bot, kamu adalah anak remaja, kamu memiliki sifat kasar dan toxic, kadang kamu memakai emoji 🧢🖕 ketika kesal, kamu kalau ngobrol menggunakan bahasa gaul anak Indonesia seperti lu untuk kamu dan gw untuk aku, kamu rest api Rioo: https://restapii.rioooxdzz.web.id, untuk nomor nya wa.me/6285691304150'
-                },
-                {
-                    from: 'you',
-                    content: message
-                },
-            ],
-            message: message,
-        };
-
-        const response = await axios.post('https://talkai.info/id/chat/send2/', data, {
-            headers
-        });
-
-    return response.data;
-  } catch (error) {
-    throw error;
+async function geminipro(options) {
+  try {
+    return await new Promise(async(resolve, reject) => {
+      options = {
+        model: "gemini-pro",
+        messages: options?.messages,
+        temperature: options?.temperature || 0.9,
+        top_p: options?.top_p || 0.7,
+        top_k: options?.top_p || 40,
+      }
+      if(!options?.messages) return reject("missing messages input payload!");
+      if(!Array.isArray(options?.messages)) return reject("invalid array in messages input payload!");
+      if(isNaN(options?.top_p)) return reject("invalid number in top_p payload!");
+      if(isNaN(options?.top_k)) return reject("invalid number in top_k payload!");
+      axios.post("https://api.acloudapp.com/v1/completions", options, {
+        headers: {
+          authorization: "sk-9jL26pavtzAHk9mdF0A5AeAfFcE1480b9b06737d9eC62c1e"
+        }
+      }).then(res => {
+        const data = res.data;
+        if(!data.choices[0].message.content) return reject("failed get response message!")
+        resolve({
+          sukses: true,
+          answer: data.choices[0].message.content
+        })
+      }).catch(reject)
+    })
+  } catch (e) {
+    return {
+      success: false,
+      errors: [e]
+    }
   }
 }
 async function gptlogic(prompt) {
@@ -1243,13 +1244,13 @@ app.get('/api/smartcontract', async (req, res) => {
   }
 });
 
-app.get('/api/talkai', async (req, res) => {
+app.get('/api/geminipro', async (req, res) => {
   try {
-    const message = req.query.message;
-    if (!message) {
+    const options = req.query.message;
+    if (!options) {
       return res.status(400).json({ error: 'Parameter "text" tidak ditemukan' });
     }
-    const response = await talkai(message);
+    const response = await geminipro(options);
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
