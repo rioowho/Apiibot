6639a0adde7ac9f7e8efbd300f5a1c5eaa5683c9
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-01 12:18:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const { run } = require('shannz-playwright');
 var { performance } = require("perf_hooks");
 const NodeCache = require('node-cache');
 const jsobfus = require('javascript-obfuscator')
-const ytdlnew = require('./lib/SaveTube')
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.enable("trust proxy");
@@ -26,7 +25,85 @@ global.creator = "@riooxdzz"
 app.use(cors());
 
 const myCache = new NodeCache({ stdTTL: 3600, checkperiod: 120 });
-
+function ytdlnew(url, format = 'mp3') {
+    return new Promise(async(resolve, reject) => {
+ 
+        const isYouTubeUrl = /^(?:(?:https?:)?\/\/)?(?:(?:(?:www|m(?:usic)?)\.)?youtu(?:\.be|be\.com)\/(?:shorts\/|live\/|v\/e(?:mbed)?\/|watch(?:\/|\?(?:\S+=\S+&)*v=)|oembed\?url=https?%3A\/\/(?:www|m(?:usic)?)\.youtube\.com\/watch\?(?:\S+=\S+&)*v%3D|attribution_link\?(?:\S+=\S+&)*u=(?:\/|%2F)watch(?:\?|%3F)v(?:=|%3D))?|www\.youtube-nocookie\.com\/embed\/)(([\w-]{11}))[\?&#]?\S*$/
+    
+        if (!isYouTubeUrl.test(url)) {
+            resolve({
+                status: false,
+                mess: "Link is not valid"
+            })
+        }
+        const id = url.match(isYouTubeUrl)?.[2]
+    
+        const hr = {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+            'Referer': 'https://id.ytmp3.mobi/',
+        }
+ 
+        const init = await axios.get(`https://d.ymcdn.org/api/v1/init?p=y&23=1llum1n471&_=${Math.random()}`, {
+            headers: hr
+        });
+ 
+        if (init.data.convertURL) {
+ 
+            let convert = await axios.get(`${init.data.convertURL}&v=${id}&f=${format}&_=${Math.random()}`, {
+                headers: hr
+            }).then(x => x.data)
+ 
+            async function progress(url, dl) {
+                let currentProgress = 0;
+                let title = '';
+ 
+                while (currentProgress < 3) {
+                    try {
+                        const response = await axios.get(url, {
+                            headers: hr
+                        });
+                        const data = response.data;
+ 
+                        if (data.error > 0) {
+                            resolve({
+                                status: false,
+                                mess: `Error: ${data.error}`
+                            });
+                        }
+ 
+                        currentProgress = data.progress;
+                        title = data.title
+ 
+                        if (currentProgress < 3) {
+                            await new Promise(resolve => setTimeout(resolve, 200));
+                        }
+                    } catch (error) {
+                        resolve({
+                            status: false,
+                            mess: 'Error checking progress:' + error.message
+                        })
+                    }
+                }
+                return { dl, title }
+            }
+ 
+            const result = await progress(convert.progressURL, convert.downloadURL);
+            resolve({
+                status: true,
+                title: result.title,
+                dl: result.dl
+            })
+        } else {
+            resolve({
+                status: false,
+                mess: "convertURL is missing"
+            })
+        }
+    })
+};
 async function obfus(query) {
 			return new Promise((resolve, reject) => {
 				try {
