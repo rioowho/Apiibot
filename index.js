4e9caf2d4e9fd72ed262fb64ab60bb490be479bc
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-02 19:00:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -27,8 +27,14 @@ async function bard(prompt) {
     const apiKey = 'AIzaSyB88NfVhPnuCKWo8mx0Q5hub52m5Vklt2o'; // Dapatkan apikey dari  https://aistudio.google.com/app/apikey
     const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
     const body = {
-                text: prompt
-            };
+        contents: {
+            {
+                parts: {
+                    { text: prompt }
+                }
+            }
+        }
+    };
     try {
         const response = await fetch(url, {
             method: 'POST',
