76db5bed28c3fa3f99aabe6ef21f330109e9c8a1
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-08 12:15:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1004,43 +1004,105 @@ async function MediaFireDl(url) {
    })
 }
 async function igdl(url) {
+  return new Promise(async (resolve) => {
   try {
-    const form = new FormData();
-    const { k_exp, k_token } = await getToken();
-
-    form.append("url", url);
-    form.append("k_token", k_token);
-    form.append("k_exp", k_exp);
-    form.append("q", url); // Check if this URL is correct
-    form.append("t", "media");
-    form.append("lang", "en");
-    form.append("v", "v2");
-
-    // Post request with form data and appropriate headers
-    const response = await axios.post(
-      "https://storysaver.to/api/ajaxSearch",
-      form,
-      { headers: form.getHeaders ? form.getHeaders() : {} }
-    );
-
-    // console.log(response.data.data);
-    const $ = cheerio.load(response.data.data);
-
-    // Find all <a> tags and extract href attributes
-    const result = [];
-    $('a').each((index, element) => {
-      const href = $(element).attr('href');
-      if (href) {
-        if (!href.includes("snapcdn")) return
-        result.push(href);
-      }
-    });
-
-    return result
-  } catch (error) {
-    console.error("Error fetching Instagram data:", error.message);
-    return error.message
+  if (!url.match(/(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/) && !url.match(/(https|http):\/\/www.instagram.com\/(p|reel|tv|stories)/gi)) return resolve({ developer: 'Rioo Xdzz', status: false, msg: `Link Url not valid` })
+  function decodeSnapApp(args) {
+  let [h, u, n, t, e, r] = args
+  // @ts-ignore
+  function decode (d, e, f) {
+  const g = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'.split('')
+  let h = g.slice(0, e)
+  let i = g.slice(0, f)
+  // @ts-ignore
+  let j = d.split('').reverse().reduce(function (a, b, c) {
+  if (h.indexOf(b) !== -1)
+  return a += h.indexOf(b) * (Math.pow(e, c))
+  }, 0)
+  let k = ''
+  while (j > 0) {
+  k = i[j % f] + k
+  j = (j - (j % f)) / f
   }
+  return k || '0'
+  }
+  r = ''
+  for (let i = 0, len = h.length; i < len; i++) {
+  let s = ""
+  // @ts-ignore
+  while (h[i] !== n[e]) {
+  s += h[i]; i++
+  }
+  for (let j = 0; j < n.length; j++)
+  s = s.replace(new RegExp(n[j], "g"), j.toString())
+  // @ts-ignore
+  r += String.fromCharCode(decode(s, e, 10) - t)
+  }
+  return decodeURIComponent(encodeURIComponent(r))
+  }
+  function getEncodedSnapApp(data) {
+  return data.split('decodeURIComponent(escape(r))}(')[1]
+  .split('))')[0]
+  .split(',')
+  .map(v => v.replace(/"/g, '').trim())
+  }
+  function getDecodedSnapSave (data) {
+  return data.split('getElementById("download-section").innerHTML = "')[1]
+  .split('"; document.getElementById("inputData").remove(); ')[0]
+  .replace(/\\(\\)?/g, '')
+  }
+  function decryptSnapSave(data) {
+  return getDecodedSnapSave(decodeSnapApp(getEncodedSnapApp(data)))
+  }
+  const html = await got.post('https://snapsave.app/action.php?lang=id', {
+  headers: {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'Content-Type': 'application/x-www-form-urlencoded','origin': 'https://snapsave.app',
+  'Referer': 'https://snapsave.app/id',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+  },
+  form: { url }
+  }).text()
+  const decode = decryptSnapSave(html)
+  const $ = cheerio.load(decode)
+  const results = []
+  if ($('table.table').length || $('article.media > figure').length) {
+  const thumbnail = $('article.media > figure').find('img').attr('src')
+  $('tbody > tr').each((_, el) => {
+  const $el = $(el)
+  const $td = $el.find('td')
+  const resolution = $td.eq(0).text()
+  let _url = $td.eq(2).find('a').attr('href') || $td.eq(2).find('button').attr('onclick')
+  const shouldRender = /get_progressApi/ig.test(_url || '')
+  if (shouldRender) {
+  _url = /get_progressApi\('(.*?)'\)/.exec(_url || '')?.[1] || _url
+  }
+  results.push({
+  resolution,
+  thumbnail,
+  url: _url,
+  shouldRender
+  })
+  })
+  } else {
+  $('div.download-items__thumb').each((_, tod) => {
+  const thumbnail = $(tod).find('img').attr('src')
+  $('div.download-items__btn').each((_, ol) => {
+  let _url = $(ol).find('a').attr('href')
+  if (!/https?:\/\//.test(_url || '')) _url = `https://snapsave.app${_url}`
+  results.push({
+  thumbnail,
+  url: _url
+  })
+  })
+  })
+  }
+  if (!results.length) return resolve({ status: false, msg: 'No Data' })
+  return resolve({ status: true, data: results })
+  } catch (e) {
+  return resolve({ status: false, msg: e.message })
+  }
+  })
 }
 class Spotify {
     async Download(url) {
