16cb74cca28a79ee29365580b5a8f08b6cc0e486
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-10 22:49:41 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -43,210 +43,7 @@ app.set("json spaces", 2);
 global.creator = "@riooxdzz"
 // Middleware untuk CORS
 app.use(cors());
-const privateKey = `-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEIF7u6fQ1RaVV7YUg+dOD0j6upkFJ1fNQZ4nzz8n0m1zboAoGCCqGSM49
-AwEHoUQDQgAEuG6npq0n2HHW+kKK2x2RfMh0J/AzwJeXgUKuMvzC2aXoKZyTLNf+
-TNX1cfH+h+aMkOhenIabeiHsjdiHzX/n54lM/g==
------END EC PRIVATE KEY-----`;
-const payload = {
-  exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7, // 1 minggu dalam detik
-  referer: "https://apimate.net/",
-  origin: "https://apimate.net",
-  userAgent:
-    "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
-};
-const header = {
-  alg: "HS256",
-  typ: "JWT",
-  kid: "8d79bd11784029a8cfb3aa5a79741a6f",
-};
-const jwt_token = jwt.sign(payload, privateKey, {
-  algorithm: "ES256",
-  header: header,
-});
 
-const ytdl = {
-  // url parser
-  parseYouTubeUrl: async function parseYouTubeUrl(url) {
-    const [domain, querystring] = url.split("?");
-    const options = {};
-    const query = qs.parse(querystring);
-
-    if (query.list) {
-      options.playlistId = query.list;
-    } else if (query.v) {
-      options.videoId = query.v;
-    } else {
-      const shortVideo = new UrlPattern(
-        "(http(s)\\://)(www.)youtu.be/:videoId",
-      );
-      const directVideo = new UrlPattern(
-        "(http(s)\\://)(www.)youtube.com/v/:videoId",
-      );
-      const embedVideo = new UrlPattern(
-        "(http(s)\\://)(www.)youtube.com/embed/:videoId",
-      );
-
-      let params = shortVideo.match(domain);
-      if (params) options.videoId = params.videoId;
-
-      params = directVideo.match(domain);
-      if (params) options.videoId = params.videoId;
-
-      params = embedVideo.match(domain);
-      if (params) options.videoId = params.videoId;
-    }
-
-    // Check for start and end times for single videos.
-    if (options.videoId) {
-      // Start times can be set with &start= for embed urls or
-      // &t= for short urls.
-      if (query.start) {
-        options.start = parseInt(query.start, 10);
-      } else if (query.t) {
-        options.start = parseInt(query.t, 10);
-      }
-
-      if (query.end) {
-        options.end = parseInt(query.end, 10);
-      }
-    }
-
-    return options;
-  },
-  getInfo: async function getInfo(url) {
-    try {
-      if (!url)
-        return { status: false, message: "failed get info, invalid url link" };
-      return await new Promise(async (resolve, reject) => {
-        const videoId = (await parseYouTubeUrl(url)).videoId;
-        axios
-          .get(`https://rr-01-bucket.cdn1313.net/api/v4/info/` + videoId, {
-            headers: {
-              authority: "rr-01-bucket.cdn1313.net",
-              accept: "application/json",
-              origin: "https://apimate.net",
-              referer: "https://apimate.net/",
-            },
-          })
-          .then(async (res) => {
-            const data = res.data;
-            if (!data?.videoId) reject("failed fetch video information");
-            let video = data.formats.video.mp4.filter((d) =>
-              /H.264/.test(d.codec),
-            );
-            resolve({
-              status: true,
-              data: {
-                videoId: data.videoId,
-                title: data.title,
-                duration: data.humanDuration,
-                media: {
-                  video,
-                  audio: data.formats.audio.mp3 || undefined,
-                },
-              },
-            });
-          })
-          .catch(reject);
-      });
-    } catch (e) {
-      return { status: false, message: e };
-    }
-  },
-  download: async function download(token) {
-    try {
-      if (!token) return { status: false, message: "invalid content token!" };
-      return await new Promise(async (resolve, reject) => {
-        axios
-          .post(
-            "https://rr-01-bucket.cdn1313.net/api/v4/convert",
-            {
-              token,
-            },
-            {
-              headers: {
-                authorization: jwt_token,
-                authority: "rr-01-bucket.cdn1313.net",
-                accept: "application/json",
-                origin: "https://apimate.net",
-                referer: "https://apimate.net/",
-                "user-agent":
-                  "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
-              },
-            },
-          )
-          .then(async (res) => {
-            const data = res.data;
-            if (data?.error) reject("content token expired!");
-            let jobProc = async () => {
-              return await new Promise((resolve, reject) => {
-                axios
-                  .get(
-                    `https://rr-01-bucket.cdn1313.net/api/v4/status/` + data.id,
-                    {
-                      headers: {
-                        authorization: jwt_token,
-                        authority: "rr-01-bucket.cdn1313.net",
-                        accept: "application/json",
-                        origin: "https://apimate.net",
-                        referer: "https://apimate.net/",
-                        "user-agent":
-                          "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
-                      },
-                    },
-                  )
-                  .then((res) => {
-                    const data = res.data;
-                    if (data?.error)
-                      reject({ status: "error", message: data.message });
-                    if (data.status === "failed")
-                      reject({
-                        status: "error",
-                        message: "failed converting content",
-                      });
-                    if (data.status == "active")
-                      resolve({
-                        status: "queue",
-                        message: "on queue...",
-                        data,
-                      });
-                    resolve({
-                      status: "completed",
-                      data: {
-                        url: data.download,
-                        title: data.title,
-                        quality: data.quality,
-                        ext: data.ext,
-                      },
-                    });
-                  })
-                  .catch((e) => reject({ status: false, message: e }));
-              });
-            };
-            let count = 0;
-            async function process() {
-              let job = await jobProc();
-              console.log(count + ":", job);
-              count++;
-              if (job?.status == "error")
-                return reject("failed converting content");
-              if (job.status == "completed") {
-                job.status = true;
-                return resolve(job);
-              }
-              setTimeout(process, 3000);
-            }
-            await process();
-          })
-          .catch(reject);
-      });
-    } catch (e) {
-      return { status: false, message: e };
-    }
-  },
-};
-    
 const hdown = {
     dl: async (link) => {
         try {
@@ -983,6 +780,7 @@ async function ytmp3(linkurl) {
       },
       {
         headers: {
+        'accept': '*/*',
            "Content-Type": "application/json",
             "Accept": "application/json",
         },
@@ -2478,11 +2276,11 @@ app.get('/api/ytmp4', async (req, res) => {
 });
 app.get('/api/ytmp3', async (req, res) => {
   try {
-    const url = req.query.url; // Ambil parameter dari query
-    if (!url) {
+    const linkurl = req.query.url; // Ambil parameter dari query
+    if (!linkurl) {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
     }
-    const result = await ytdl.getInfo(url);
+    const result = await ytmp3(linkurl);
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
