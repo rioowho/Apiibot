f9c3d078022ba1fdda0a2beb9b6fa36222f9808b
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-11 18:25:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -928,9 +928,11 @@ async function ytmp3(linkurl) {
       },
       {
         headers: {
-        'accept': '*/*',
+         'Accept': 'application/json, text/plain, */*',
            "Content-Type": "application/json",
             "Accept": "application/json",
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+            'Referer': 'https://c.blahaj.ca/',
         },
       }
     );
