4a2b88cfa171384179df8d2d7e054f7149f4c501
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-12 08:00:32 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -43,90 +43,150 @@ app.set("json spaces", 2);
 global.creator = "@riooxdzz"
 // Middleware untuk CORS
 app.use(cors());
-const urls = {
-    info: 'https://m8.fly.dev/api/info',
-    download: 'https://m8.fly.dev/api/download'
-}
+const formats = ["audio", "video"];
+const audioQuality = [320, 256, 192, 128, 64];
+const videoQuality = ["360p", "480p", "720p", "1080p"];
 
-const getHeaders = (extraHeaders = {}) => ({
-    'Content-Type': 'application/json',
-    'User-Agent': 'Downloader/1.0.0',
-    'Referer': 'https://ytiz.xyz/',
-    ...extraHeaders
-})
+const ytdl = async (url) => {
+  const getToken = async (url) => {
+    const extractVideoId = (url) => {
+      const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+      const match = url.match(regex);
+      return match ? match[1] : null;
+    };
 
-const sendRequest = async (urlKey, data) => {
-    const response = await axios.post(urls[urlKey], data, { headers: getHeaders() })
-    return response.data
-}
-
-const validateInput = (format, quality) => {
-    const validFormats = ['m4a', 'mp3', 'flac']
-    const validQualities = ['32', '64', '128', '192', '256', '320']
-
-    if (!validFormats.includes(format)) {
-        throw new Error(`Invalid format! Choose one of the following: ${validFormats.join(', ')}`)
+    const id = extractVideoId(url);
+    if (!id) {
+      throw new Error('ID videonya gk ketemu jir, pastikan link youtube yak');
     }
 
-    if (!validQualities.includes(quality)) {
-        throw new Error(`Invalid quality! Choose one of the following: ${validQualities.join(', ')}`)
+    const config = {
+      method: 'GET',
+      url: `https://dd-n01.yt2api.com/api/v4/info/${id}`,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Android 10; Mobile; rv:131.0) Gecko/131.0 Firefox/131.0',
+        'Accept': 'application/json',
+        'accept-language': 'id-ID',
+        'referer': 'https://bigconv.com/',
+        'origin': 'https://bigconv.com',
+        'alt-used': 'dd-n01.yt2api.com',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'priority': 'u=0',
+        'te': 'trailers'
+      }
+    };
+
+    const response = await axios.request(config);
+    const cookies = response.headers['set-cookie'];
+    const processedCookie = cookies ? cookies[0].split(';')[0] : '';
+    const authorization = response.headers['authorization'] || '';
+    const result = { data: response.data, cookie: processedCookie, authorization };
+    return result;
+  };
+
+  const convert = async (url, format, quality = "720p") => {
+    const data = await getToken(url);
+    const formats = data.data.formats;
+
+    let token;
+    if (format === "audio") {
+      const audioOptions = formats.audio.mp3;
+      const selectedAudio = audioOptions.find(option => option.quality === quality);
+      if (selectedAudio) {
+        token = selectedAudio.token;
+      } else {
+        throw new Error(`Kualitas audio ${quality} tidak tersedia.`);
+      }
+    } else if (format === "video") {
+      const videoOptions = formats.video.mp4;
+      const selectedVideo = videoOptions.find(option => option.quality === quality);
+      if (selectedVideo) {
+        token = selectedVideo.token;
+      } else {
+        throw new Error(`Kualitas video ${quality} tidak tersedia.`);
+      }
+    } else {
+      throw new Error('Format tidak dikenali. Gunakan "audio" atau "video".');
     }
-}
 
-const ensureDirectoryExists = (filePath) => {
-    const directoryPath = path.dirname(filePath)
-    if (!fs.existsSync(directoryPath)) {
-        fs.mkdirSync(directoryPath, { recursive: true })
-    }
-}
+    const raw = JSON.stringify({ "token": token });
 
-const fetchVideoDetails = async (url, format) => sendRequest('info', { url, format, startTime: 0, endTime: 0 })
+    const config = {
+      method: 'POST',
+      url: 'https://dd-n01.yt2api.com/api/v4/convert',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Android 10; Mobile; rv:131.0) Gecko/131.0 Firefox/131.0',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'accept-language': 'id-ID',
+        'referer': 'https://bigconv.com/',
+        'origin': 'https://bigconv.com',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'priority': 'u=0',
+        'te': 'trailers',
+        'Cookie': data.cookie,
+        'authorization': data.authorization
+      },
+      data: raw
+    };
 
-const fetchAudio = async (url, quality, filename, randomID, format) => sendRequest('download', {
-    url,
-    quality,
-    metadata: true,
-    filename,
-    randID: randomID,
-    trim: false,
-    startTime: 0,
-    endTime: 0,
-    format
-})
+    const response = await axios.request(config);
+    return { jobId: response.data.id, cookie: data.cookie, authorization: data.authorization };
+  };
 
-const executeDownload = async (url, format = 'mp3', quality = '128') => {
-    validateInput(format, quality)
+  const download = async (url, format, quality = "720p") => {
+    const { jobId, cookie, authorization } = await convert(url, format, quality);
+    return new Promise((resolve, reject) => {
+      const checkStatus = async () => {
+        const config = {
+          method: 'GET',
+          url: `https://dd-n01.yt2api.com/api/v4/status/${jobId}`,
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (Android 10; Mobile; rv:131.0) Gecko/131.0 Firefox/131.0',
+            'Accept': 'application/json',
+            'accept-language': 'id-ID',
+            'referer': 'https://bigconv.com/',
+            'origin': 'https://bigconv.com',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+            'priority': 'u=4',
+            'te': 'trailers',
+            'Cookie': cookie,
+            'authorization': authorization
+          }
+        };
 
-    const videoDetails = await fetchVideoDetails(url, format)
-    const audioDetails = await fetchAudio(url, quality, videoDetails.filename, videoDetails.randID, format)
-    console.log(audioDetails)
-
-    const outputDirectory = path.join(process.cwd(), 'downloads')
-    const outputPath = path.join(outputDirectory, audioDetails.filename)
-    ensureDirectoryExists(outputPath)
-
-    const fileResponse = await axios.post('https://m8.fly.dev/api/file_send', {
-        filepath: audioDetails.filepath,
-        randID: audioDetails.randID
-    }, { responseType: 'arraybuffer' })
-
-    fs.writeFileSync(outputPath, fileResponse.data)
-    console.log(`${outputPath}`)
-}
-
-const YTDownloader = {
-    download: async (url, format = 'mp3', quality = '32') => {
-        try {
-            await executeDownload(url, format, quality)
-        } catch (error) {
-            console.error('Error downloading file:', error.message)
+        const response = await axios.request(config);
+        if (response.data.status === 'completed') {
+          clearInterval(interval);
+          resolve(response.data);
+        } else if (response.data.status === 'failed') {
+          clearInterval(interval);
+          resolve(response.data);
         }
+      };
+
+      const interval = setInterval(checkStatus, 5000);
+    });
+  };
+
+  try {
+    const result = await download(url, "video", "720p");
+    return {result};
+    } catch (error) {
+        console.error("Error during the API call:", error);
+        return "Terjadi kesalahan saat memproses permintaan Anda. Mohon coba lagi nanti. 😔";
     }
-}
+};
 async function metaai(text, userName) {
     const Together = require("together-ai")
     const together = new Together({ 
-            apiKey: '522aeeed9ccfea4eeabb86608d24bcc0ad35b0c08598c60bdf214b8bd7bb42c0, b08ecc42947ad25e0acdc8a508db4e6be52cfebb8910dd36a8abef8406b91c20' 
+            apiKey: '522aeeed9ccfea4eeabb86608d24bcc0ad35b0c08598c60bdf214b8bd7bb42c0' 
         });
 
     // Fallback to 'User' if userName is not provided
@@ -1364,7 +1424,7 @@ function aio(link) {
   });
 }
 
-async function ytdl(videoUrl) {
+async function ytdll(videoUrl) {
  const form = new FormData();
  form.append('query', videoUrl);
 
@@ -2529,7 +2589,7 @@ app.get('/api/ytmp4', async (req, res) => {
     if (!url) {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
     }
-    const response = await YTDownloader.download(url);
+    const response = await ytdl.download(url);
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
