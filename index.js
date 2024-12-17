38a263dd656bff75774b5f314a753236d93d9714
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-17 17:09:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -36,7 +36,31 @@ app.set("json spaces", 2);
 global.creator = "@riooxdzz"
 // Middleware untuk CORS
 app.use(cors());
-
+async function stickersearch(query) {
+return new Promise((resolve) => {
+axios.get(`https://getstickerpack.com/stickers?query=${query}`).then(({ data }) => {
+const $ = cheerio.load(data)
+const link = [];
+$('#stickerPacks > div > div:nth-child(3) > div > a').each(function(a, b) {
+link.push($(b).attr('href'))
+})
+let rand = link[Math.floor(Math.random() * link.length)]
+axios.get(rand).then(({data}) => {
+const $$ = cheerio.load(data)
+const url = [];
+$$('#stickerPack > div > div.row > div > img').each(function(a, b) {
+url.push($$(b).attr('src').split('&d=')[0])
+})
+resolve({
+title: $$('#intro > div > div > h1').text(),
+author: $$('#intro > div > div > h5 > a').text(),
+author_link: $$('#intro > div > div > h5 > a').attr('href'),
+sticker: url
+})
+})
+})
+})
+}
 async function sendEmail(recipientEmail, text) {
     // Konfigurasi transporter
     const transporter = nodemailer.createTransport({
@@ -1443,89 +1467,104 @@ var durationMultipliers = {
 
 function youtubeSearch(query) {
   return new Promise((resolve, reject) => {
-    axios("https://m.youtube.com/results?search_query=" + query, { method: "GET", headers: { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36' } }).then(({ data }) => {
-      const $ = cheerio.load(data)
-      var sc;
-      $('script').map(function () {
+    axios("https://m.youtube.com/results?search_query=" + query, { 
+      method: "GET", 
+      headers: { 
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36' 
+      } 
+    }).then(({ data }) => {
+      const $ = cheerio.load(data);
+      let sc;
+      $('script').each(function () {
         const el = $(this).html();
         let regex;
         if ((regex = /var ytInitialData = /gi.exec(el || ''))) {
           sc = JSON.parse(regex.input.replace(/^var ytInitialData = /i, '').replace(/;$/, ''));
         }
-        return regex && sc;
       });
-      var results = { video: ", ", channel: ", ", playlist: ", " };
+
+      let results = { video: [], channel: [], playlist: [] };
+
       sc.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents.forEach((v) => {
-        var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x, _y, _z, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13;
         const typeName = Object.keys(v)[0];
         const result = v[typeName];
-        if (['horizontalCardListRenderer', 'shelfRenderer'].includes(typeName)) {
-          return;
-        }
+
+        if (['horizontalCardListRenderer', 'shelfRenderer'].includes(typeName)) return;
+
         const isChannel = typeName === 'channelRenderer';
         const isVideo = typeName === 'videoRenderer';
         const isMix = typeName === 'radioRenderer';
-        //===[ Filtering ]===\\
+
+        // Filtering Video
         if (isVideo) {
-          const view = ((_a = result.viewCountText) === null || _a === void 0 ? void 0 : _a.simpleText) || ((_b = result.shortViewCountText) === null || _b === void 0 ? void 0 : _b.simpleText) || ((_d = (_c = result.shortViewCountText) === null || _c === void 0 ? void 0 : _c.accessibility) === null || _d === void 0 ? void 0 : _d.accessibilityData.label);
-          const _duration = (_f = (_e = result.thumbnailOverlays) === null || _e === void 0 ? void 0 : _e.find((v) => Object.keys(v)[0] === 'thumbnailOverlayTimeStatusRenderer')) === null || _f === void 0 ? void 0 : _f.thumbnailOverlayTimeStatusRenderer.text;
+          const view = result.viewCountText?.simpleText || result.shortViewCountText?.simpleText || result.shortViewCountText?.accessibility?.accessibilityData.label;
+          const _duration = result.thumbnailOverlays?.find((v) => Object.keys(v)[0] === 'thumbnailOverlayTimeStatusRenderer')?.thumbnailOverlayTimeStatusRenderer.text;
           const videoId = result.videoId;
-          const duration = ((_g = result.lengthText) === null || _g === void 0 ? void 0 : _g.simpleText) || (_duration === null || _duration === void 0 ? void 0 : _duration.simpleText);
+          const duration = result.lengthText?.simpleText || _duration?.simpleText;
+          
           let durationS = 0;
-          (_h = ((duration === null || duration === void 0 ? void 0 : duration.split('.').length) && duration.indexOf(':') === -1 ? duration.split('.') : duration === null || duration === void 0 ? void 0 : duration.split(':'))) === null || _h === void 0 ? void 0 : _h.forEach((v, i, arr) => (durationS += durationMultipliers[arr.length]['' + i] * parseInt(v)));
+          if (duration) {
+            const durationArray = duration.split(':');
+            durationArray.forEach((v, i, arr) => {
+              durationS += durationMultipliers[arr.length][i] * parseInt(v);
+            });
+          }
+
           results.video.push({
-            authorName: (_l = (((_j = result.ownerText) === null || _j === void 0 ? void 0 : _j.runs) || ((_k = result.longBylineText) === null || _k === void 0 ? void 0 : _k.runs) || [])[0]) === null || _l === void 0 ? void 0 : _l.text,
-            authorAvatar: (_p = (_o = (_m = result.channelThumbnailSupportedRenderers) === null || _m === void 0 ? void 0 : _m.channelThumbnailWithLinkRenderer.thumbnail.thumbnails) === null || _o === void 0 ? void 0 : _o.filter(({ url }) => url)) === null || _p === void 0 ? void 0 : _p.pop().url,
+            authorName: result.ownerText?.runs[0]?.text || result.longBylineText?.runs[0]?.text,
+            authorAvatar: result.channelThumbnailSupportedRenderers?.channelThumbnailWithLinkRenderer.thumbnail.thumbnails?.pop()?.url,
             videoId,
             url: encodeURI('https://www.youtube.com/watch?v=' + videoId),
             thumbnail: result.thumbnail.thumbnails.pop().url,
-            title: (_t = (((_r = (_q = result.title) === null || _q === void 0 ? void 0 : _q.runs.find((v) => v.text)) === null || _r === void 0 ? void 0 : _r.text) || ((_s = result.title) === null || _s === void 0 ? void 0 : _s.accessibility.accessibilityData.label))) === null || _t === void 0 ? void 0 : _t.trim(),
-            description: (_y = (_x = (_w = (_v = (_u = result.detailedMetadataSnippets) === null || _u === void 0 ? void 0 : _u[0]) === null || _v === void 0 ? void 0 : _v.snippetText.runs) === null || _w === void 0 ? void 0 : _w.filter(({ text }) => text)) === null || _x === void 0 ? void 0 : _x.map(({ text }) => text)) === null || _y === void 0 ? void 0 : _y.join(''),
-            publishedTime: (_z = result.publishedTimeText) === null || _z === void 0 ? void 0 : _z.simpleText,
-            durationH: ((_0 = result.lengthText) === null || _0 === void 0 ? void 0 : _0.accessibility.accessibilityData.label) || (_duration === null || _duration === void 0 ? void 0 : _duration.accessibility.accessibilityData.label),
+            title: result.title?.runs.find((v) => v.text)?.text || result.title?.accessibility?.accessibilityData.label,
+            description: result.detailedMetadataSnippets?.[0]?.snippetText?.runs?.map(({ text }) => text).join(''),
+            publishedTime: result.publishedTimeText?.simpleText,
+            durationH: result.lengthText?.accessibility?.accessibilityData?.label || _duration?.accessibility?.accessibilityData?.label,
             durationS,
             duration,
             viewH: view,
-            view: (_1 = (((view === null || view === void 0 ? void 0 : view.indexOf('x')) === -1 ? view === null || view === void 0 ? void 0 : view.split(' ')[0] : view === null || view === void 0 ? void 0 : view.split('x')[0]) || view)) === null || _1 === void 0 ? void 0 : _1.trim(),
+            view: view?.split('x')[0]?.trim(),
             type: typeName.replace(/Renderer/i, '')
           });
         }
+
+        // Filtering Channel
         if (isChannel) {
           const channelId = result.channelId;
-          //const _subscriber = ((_2 = result.subscriberCountText) === null || _2 === void 0 ? void 0 : _2.accessibility.accessibilityData.label) || ((_3 = result.subscriberCountText) === null || _3 === void 0 ? void 0 : _3.simpleText);
           results.channel.push({
             channelId,
             url: encodeURI('https://www.youtube.com/channel/' + channelId),
-            channelName: result.title.simpleText || ((_5 = (_4 = result.shortBylineText) === null || _4 === void 0 ? void 0 : _4.runs.find((v) => v.text)) === null || _5 === void 0 ? void 0 : _5.text),
-            avatar: 'https:' + ((_6 = result.thumbnail.thumbnails.filter(({ url }) => url)) === null || _6 === void 0 ? void 0 : _6.pop().url),
-            isVerified: ((_7 = result.ownerBadges) === null || _7 === void 0 ? void 0 : _7.pop().metadataBadgeRenderer.style) === 'BADGE_STYLE_TYPE_VERIFIED',
-            subscriberH: result.videoCountText ? result.videoCountText.simpleText : "",
-            subscriber: result.videoCountText ? result.videoCountText.simpleText.split(" ")[0] : "",
-            description: (_13 = (_12 = (_11 = (_10 = result.descriptionSnippet) === null || _10 === void 0 ? void 0 : _10.runs) === null || _11 === void 0 ? void 0 : _11.filter(({ text }) => text)) === null || _12 === void 0 ? void 0 : _12.map(({ text }) => text)) === null || _13 === void 0 ? void 0 : _13.join(''),
+            channelName: result.title?.simpleText || result.shortBylineText?.runs.find((v) => v.text)?.text,
+            avatar: 'https:' + result.thumbnail.thumbnails?.filter(({ url }) => url)?.pop()?.url,
+            isVerified: result.ownerBadges?.pop()?.metadataBadgeRenderer?.style === 'BADGE_STYLE_TYPE_VERIFIED',
+            subscriberH: result.videoCountText?.simpleText || "",
+            subscriber: result.videoCountText?.simpleText.split(" ")[0] || "",
+            description: result.descriptionSnippet?.runs?.map(({ text }) => text).join(''),
             type: typeName.replace(/Renderer/i, '')
           });
         }
+
+        // Filtering Playlist (Mix)
         if (isMix) {
           results.playlist.push({
             playlistId: result.playlistId,
             title: result.title.simpleText,
             thumbnail: result.thumbnail.thumbnails.pop().url,
-            video: result.videos.map(({ childVideoRenderer }) => {
-              return {
-                url: encodeURI('https://www.youtube.com/watch?v=' + childVideoRenderer.videoId + "&list=" + result.playlistId),
-                videoId: childVideoRenderer.videoId,
-                title: childVideoRenderer.title.simpleText,
-                durationH: childVideoRenderer.lengthText.accessibility.accessibilityData.label,
-                duration: childVideoRenderer.lengthText.simpleText
-              };
-            }),
+            video: result.videos.map(({ childVideoRenderer }) => ({
+              url: encodeURI('https://www.youtube.com/watch?v=' + childVideoRenderer.videoId + "&list=" + result.playlistId),
+              videoId: childVideoRenderer.videoId,
+              title: childVideoRenderer.title.simpleText,
+              durationH: childVideoRenderer.lengthText.accessibility.accessibilityData.label,
+              duration: childVideoRenderer.lengthText.simpleText
+            })),
             type: 'mix'
           });
         }
-      })
-      resolve(results)
-    })
-  })
+      });
+
+      resolve(results);
+    }).catch(reject);
+  });
 }
 
 async function sfileSearch(query, page = 1) {
@@ -3024,6 +3063,22 @@ app.get('/api/search-tiktok', async (req, res) => {
       return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
     }
     const response = await tiktoks(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get('/api/search-sticker', async (req, res) => {
+  try {
+    const query = req.query.text;
+    if (!query) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await stickersearch(query);
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
