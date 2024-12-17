e43b2567dc727b3e5798517b37149909ac5d9a4b
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-17 19:27:55 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -37,7 +37,105 @@ app.set("json spaces", 2);
 global.creator = "@riooxdzz"
 // Middleware untuk CORS
 app.use(cors());
+class Claude {
+  constructor(cookie) {
+    this.cookie = cookie;
+    this.organizationId = undefined;
+  }
 
+  getHeaders() {
+    return {
+      accept: "text/event-stream, text/event-stream",
+      "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+      baggage:
+        "sentry-environment=production,sentry-release=7056d48863ef1ff17036d5f9a9ce84133c63abfc,sentry-public_key=58e9b9d0fc244061a1b54fe288b0e483,sentry-trace_id=963e5c5cde6a43219b5c61e9fd8b5be6",
+      "content-type": "application/json",
+      "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120"',
+      "sec-ch-ua-mobile": "?1",
+      "sec-ch-ua-platform": '"Android"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "sentry-trace": "963e5c5cde6a43219b5c61e9fd8b5be6-b9f5b45af75131c2-0",
+      cookie: this.cookie,
+      Referer: "https://claude.ai/chats",
+    };
+  }
+
+  getOrganizationId() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await fetch("https://claude.ai/api/organizations", {
+          headers: this.getHeaders(),
+        });
+        const res = await response.json();
+      //  const uuid = res[0].uuid;
+        resolve(res);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  create() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const organizationId = await this.getOrganizationId();
+        this.organizationId = organizationId;
+        const response = await fetch(
+          `https://claude.ai/api/organizations/4cc4ea19-f8da-4afa-8a97-ce3e15c61d6d/chat_conversations`,
+          {
+            headers: this.getHeaders(),
+            method: "POST",
+            body: JSON.stringify({
+              uuid: uuidv4(),
+              name: "",
+            }),
+          },
+        );
+        const res = await response.json();
+        resolve(res);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  chat(text) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const chat = await this.create();
+        const response = await fetch(
+          `https://claude.ai/api/organizations/4cc4ea19-f8da-4afa-8a97-ce3e15c61d6d/chat_conversations/de37331e-19d3-4bb6-9e39-18f9400b9ac1/completion`,
+          {
+            headers: this.getHeaders(),
+            method: "POST",
+            body: JSON.stringify({
+              prompt: text,
+              timezone: "Asia/Jakarta",
+              model: "claude-2.1",
+              attachments: [],
+              files: [],
+            }),
+          },
+        );
+        const data = await response.text();
+        console.log(data)
+        const regex = /"completion":"(.*?)"/g;
+        let matches = [];
+        let match;
+
+        while ((match = regex.exec(data)) !== null) {
+          matches.push(match[1]);
+        }
+        const textResult = matches.join("").replace(/ +/g, " ").trim();
+        resolve({result: textResult.replace(/\\n/g, '\n')});
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+}
 async function gpt35turbo(inputValue) {
     try {
         const chatApiUrl = 'https://api.chatanywhere.com.cn/v1/chat/completions';
@@ -2823,6 +2921,22 @@ app.get('/api/bingimg', async (req, res) => {
       return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
     }
     const response = await bingimg(keyword);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get('/api/claude', async (req, res) => {
+  try {
+    const text = req.query.text;
+    if (!text) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await Claude.chat(text);
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
