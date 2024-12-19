95328c2812ab777f40be001b41edf1d5639e043d
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-20 06:12:38 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3675,7 +3675,7 @@ app.get('/api/mediafire', async (req, res) => {
     if (!url) {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
     }
-    const response = await MediaFireh(url);
+    const response = await mediafire(url);
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
