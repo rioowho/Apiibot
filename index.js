7a34360566ed1156ddc94eee18edad05f4f56dd6
RiooBjirr <188498615+rioowho@users.noreply.github.com>
2024-12-22 05:37:10 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3150,7 +3150,7 @@ app.get('/', (req, res) => {
 	res.sendFile(__path + '/views/home.html');
 });
 app.get('/docs', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + p '/views/index.html')
 })
 
 app.get('/chatgpt', (req, res) => {
