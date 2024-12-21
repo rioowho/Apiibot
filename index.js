dc05513f905f2ea8871ea57ea9c0d0becd4054c4
riooxdzz <167007070+RiooAjah@users.noreply.github.com>
2024-12-21 16:32:01 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3088,7 +3088,7 @@ app.get('/', (req, res) => {
 	res.sendFile(__path + '/views/home.html');
 });
 app.get('/docs', (req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html')
+    res.sendFile(__path + '/views/index.html')
 })
 
 app.get('/chatgpt', (req, res) => {
