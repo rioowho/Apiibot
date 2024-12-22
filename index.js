2f4d3e6499af885221c0b2a6880fb7175f2bedc1
RiooBjirr <188498615+rioowho@users.noreply.github.com>
2024-12-22 08:29:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3865,7 +3865,7 @@ app.get('/api/ytmp3', async (req, res) => {
     if (!url) {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
     }
-  const response = await SaveTube.dl(url, type = '4');
+  const response = await SaveTube.dl(url, type = '1');
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
