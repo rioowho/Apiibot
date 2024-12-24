175ac6e292c20a1f64a8a0d8f841e1ef3f37be91
RiooBjirr <188498615+rioowho@users.noreply.github.com>
2024-12-24 20:19:42 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3797,7 +3797,7 @@ app.get('/api/ytmp3', async (req, res) => {
     if (!url) {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
     }
-  const response = await ddownr.download(url, format);
+  const response = await ddownr.download(url, format = "webm");
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
