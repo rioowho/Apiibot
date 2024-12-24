5467c249fe580cc834400b989ca8985879dc6035
RiooBjirr <188498615+rioowho@users.noreply.github.com>
2024-12-24 20:12:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -39,7 +39,69 @@ app.set("json spaces", 2);
 global.creator = "@riooxdzz"
 // Middleware untuk CORS
 app.use(cors());
+const formatAudio = ['mp3', 'm4a', 'webm', 'acc', 'flac', 'opus', 'ogg', 'wav'];
+const formatVideo = ['360', '480', '720', '1080', '1440', '4k'];
 
+const ddownr = {
+  download: async (url, format) => {
+    if (!formatAudio.includes(format) && !formatVideo.includes(format)) {
+        throw new Error('Format nya gk support wak, coba cek lagi listnya.');
+    }
+
+    const config = {
+        method: 'GET',
+        url: `https://p.oceansaver.in/ajax/download.php?format=${format}&url=${encodeURIComponent(url)}&api=dfcb6d76f2f6a9894gjkege8a4ab232222`,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+    };
+
+    try {
+        const response = await axios.request(config);
+
+        if (response.data && response.data.success) {
+            const { id, title, info } = response.data;
+            const { image } = info;
+            const downloadUrl = await ddownr.cekProgress(id);
+
+            return {
+                id: id,
+                image: image,
+                title: title,
+                downloadUrl: downloadUrl
+            };
+        } else {
+            throw new Error('Failed to fetch video details.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+  },
+  cekProgress: async (id) => {
+    const config = {
+        method: 'GET',
+        url: `https://p.oceansaver.in/ajax/progress.php?id=${id}`,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+    };
+
+    try {
+        while (true) {
+            const response = await axios.request(config);
+
+            if (response.data && response.data.success && response.data.progress === 1000) {
+                return response.data.download_url;
+            }           
+            await new Promise(resolve => setTimeout(resolve, 5000));
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+  }
+}
 // Fungsi untuk mengambil data
 async function downloadMP3(url) {
     let result; // Deklarasi variabel result
@@ -3735,7 +3797,7 @@ app.get('/api/ytmp3', async (req, res) => {
     if (!url) {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
     }
-  const response = await downloadMP3(url);
+  const response = await ddownr.download(url, format = 'mp3');
     res.status(200).json({
       status: 200,
       creator: "RiooXdzz",
